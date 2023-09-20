c2b99479ae644ffbc45793277a9e4ab67ab4185b
Valmir Barbosa <valmir.php@gmail.com>
2023-09-20 18:46:11 -0300

    fix: HttpClient adjusts headers

--- lib/clients/http.client.ts
+++ lib/clients/http.client.ts
@@ -6,11 +6,15 @@ export class HttpClient extends AbstractClient {
   public async request(
     opts: InjectOptions | string,
   ): Promise<AssertResponseHttp> {
-    if (typeof opts !== 'string' && opts['headers']) {
-      opts['headers'] = Object.assign({}, this.config.headers, opts['headers']);
-    }
+    const _opts: InjectOptions =
+      typeof opts === 'string'
+        ? { url: opts, headers: this.config.headers }
+        : {
+            ...opts,
+            headers: Object.assign({}, this.config.headers, opts.headers),
+          };
 
-    const response = await this.app.inject(opts);
+    const response = await this.app.inject(_opts);
 
     this.reset();
     return new AssertResponseHttp(response);
